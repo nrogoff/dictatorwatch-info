f66212c46537e07f437f27204f401f8007f361fb
Nicholas Rogoff <nicholas@rogoff.uk.com>
2024-12-27 17:30:00 +0000

    feat: restructure main page with new header component and remove old App component

---
+++ src/main-page/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo192.png";
+
+const Header = ({ subtitle }) => (
+  <header className="row">
+    <div className="col-md-7">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+    <div className="col-md-7 title">
+      <h1>Dictator Watch</h1>
+    </div>
+    <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
+  </header>
+);
+
+export default Header;
